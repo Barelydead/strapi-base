75a4fbe79cb6b228c54a6fb69c732840acc4510a
Christofer Jungberg <christofer.jungberg@gmail.com>
2021-10-17 20:55:13 +0200

    removed exported template

--- tests/landingpage/index.js
+++ tests/landingpage/index.js
@@ -3,12 +3,12 @@ const request = require('supertest');
 
 it('Check valid reponse and data from /landingpages/1', async (done) => {
   // Sends GET Request to /landingpages/1 endpoint
-  const res = await request(strapi.server).get('/landingpages/1')
+  const res = await request(strapi.server).get('/landingpages/1');
 
-  expect(res.statusCode).toBe(200)
-  expect(res.body.title).toBe('Touch down!')
+  expect(res.statusCode).toBe(200);
+  expect(res.body.title).toBe('Touch down!');
 
-  done()
+  done();
 });
 
 it('Generates unique slug', async () => {
